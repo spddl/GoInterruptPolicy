8a267f6c72cf958632d9748c961c39ca95552887
spddl <spddlhh@gmail.com>
2025-04-03 08:50:51 +0200

    adding  Intel i9 13900 Without HT

--- cpusetinformation_fake.go
+++ cpusetinformation_fake.go
@@ -2,7 +2,9 @@
 
 package main
 
-import "log"
+import (
+	"log"
+)
 
 // AMD 5900x
 func Fake5900x() {
@@ -50,6 +52,26 @@ func Fake13900() {
 			lastCoreIndex = byte(i)
 		}
 		if i < 16 {
+			cs.EfficiencyClass = 1
+		}
+	}
+	SystemCpuSets = cpuSet[:count]
+}
+
+// Intel i9 13900 Without HT
+func Fake13900WithoutHT() {
+	log.Println("Intel i9 13900 Fake13900WithoutHT")
+	size := 0x20 * 64
+	cpuSet := make([]SYSTEM_CPU_SET_INFORMATION, size)
+	cpuSet[0].Size = 32
+	var count = 768
+	var index = 0x100
+	for i := 0; i < count; i++ {
+		cs := cpuSet[i].CpuSet()
+		cs.Id = uint32(index + i)
+		cs.LogicalProcessorIndex = byte(i)
+		cs.CoreIndex = byte(i)
+		if i < 8 {
 			cs.EfficiencyClass = 1
 		}
 	}
