ec98d32324b3b3134d70099a4a4a89ed99b2812d
spddl <spddlhh@gmail.com>
2025-04-03 08:53:58 +0200

    Update run.bat

--- run.bat
+++ run.bat
@@ -2,16 +2,22 @@
 
 SET filename=GoInterruptPolicy
 
+net session >nul 2>&1
+set nilIsAdmin=%errorLevel%
+
 :loop
 cls
 
-@REM gocritic check -enableAll -disable="#experimental,#opinionated,#commentedOutCode" ./...
-go build -tags debug -buildvcs=false -o %filename%_debug.exe
+if %nilIsAdmin% EQU 0 (
+    go run -tags debug -buildvcs=false .
+) else (
+    go build -tags debug -buildvcs=false -o %filename%_debug.exe
+    IF %ERRORLEVEL% EQU 0 %filename%_debug.exe
+)
 
 @REM IF %ERRORLEVEL% EQU 0 %filename%.exe -devobj \Device\NTPNP_PCI0015 -policy 4 -cpu 1,2,3 -restart
 @REM IF %ERRORLEVEL% EQU 0 %filename%.exe -devobj \Device\NTPNP_PCI0015 -policy 4 -cpu 1,2,3,4 -restart-on-change
 @REM IF %ERRORLEVEL% EQU 0 %filename%.exe -devobj \Device\NTPNP_PCI0015 -msisupported 0
-IF %ERRORLEVEL% EQU 0 %filename%_debug.exe
 
 pause
 goto loop
