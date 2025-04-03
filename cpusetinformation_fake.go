b707e17e79fab470bbb3a0835ca33edc119edba5
spddl <spddlhh@gmail.com>
2025-04-03 08:51:22 +0200

    adding Intel Core i5-13600KF

--- cpusetinformation_fake.go
+++ cpusetinformation_fake.go
@@ -142,4 +142,29 @@ func Fake2CCD12CoreHT() {
 	}
 	SystemCpuSets = cpuSet[:count]
 }
+
 // Intel Core i5-13600KF
+func Fake13600KF() {
+	log.Println("Intel i5 13600KF")
+	size := 0x20 * 64
+	cpuSet := make([]SYSTEM_CPU_SET_INFORMATION, size)
+	cpuSet[0].Size = 32
+	var lastCoreIndex byte
+	var count = 640
+	var index = 0x100
+	for i := 0; i < count; i++ {
+		cs := cpuSet[i].CpuSet()
+		cs.Id = uint32(index + i)
+		cs.LogicalProcessorIndex = byte(i)
+		if i < 12 && i%2 != 0 {
+			cs.CoreIndex = lastCoreIndex
+		} else {
+			cs.CoreIndex = byte(i)
+			lastCoreIndex = byte(i)
+		}
+		if i < 12 {
+			cs.EfficiencyClass = 1
+		}
+	}
+	SystemCpuSets = cpuSet[:count]
+}
